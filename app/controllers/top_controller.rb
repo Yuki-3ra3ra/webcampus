b164441e221dadd3d3a4d2871d5ee3cc0eefa53c
佐々木雄紀 <sasakiyuki3ra3ra@gmail.com>
2018-03-18 15:52:55 +0900

    修正(ログイン可否によるページの遷移)
    Student もしくは Userがログインしていない場合、top#indexを表示するように修正

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,11 +1,15 @@
 class TopController < ApplicationController
 
-  before_action :authenticate_user!, only: [:show]
+  #before_action :authenticate_user!, only: [:show]
 
   def index
   end
 
   def show
+    if user_signed_in? || student_signed_in?
+    else
+      redirect_to top_index_path
+    end
   end
 
 
